ed743a06c9e7e5a36adfc56bdbcd85f499d384f5
waynelau <waynelau521@gmail.com>
2014-09-18 08:44:13 +0800

    HTML 遮罩显示工具栏

---
+++ js/MaskDisplay.js
@@ -0,0 +1,15 @@
+/**
+ * 照片管理
+ * waylau.com 2014-9-11
+ */
+
+
+//鼠标移入移出事件
+
+var handlerIn = function( event ){
+    document.getElementById('toolbar').style.display ='block';
+};
+
+var handlerOut = function( event ){
+    document.getElementById('toolbar').style.display ='none';
+};
